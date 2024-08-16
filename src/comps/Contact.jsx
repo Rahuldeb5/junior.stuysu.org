3e4630c8e5ee0feff4e277dec6b5de91743cb5dd
Rahuldeb5 <rahuldeb.usa@gmail.com>
2024-08-16 09:15:55 -0400

    Fixed sticky footer

--- src/comps/Contact.jsx
+++ src/comps/Contact.jsx
@@ -13,10 +13,31 @@ const Contact = () => {
           >
             <img src={"/images/instagram.png"} alt="Instagram logo" />
           </Link>
+          <Link
+            href="https://www.facebook.com/groups/stuyclass2026"
+            className="logo-ref"
+          >
+            <img src={"/images/facebook.png"} alt="Facebook logo" />
+          </Link>
+          <Link
+            href="https://github.com/Rahuldeb5/junior.stuysu.org"
+            className="logo-ref"
+          >
+            <img src={"/images/github.png"} alt="GitHub logo" />
+          </Link>
         </Box>
       </Box>
       <Typography variant="body1" align="center">
-        © 2024 Junior Caucus. Vanna & Philip.
+        © 2024 Junior Caucus.
+      </Typography>
+      <Typography variant="body1" align="center">
+        Co-Presidents: Vanna Lei & Philip Zhang
+      </Typography>
+      <Typography variant="body1" align="center">
+        Chief of Staff: Amy Zhou & Kelly Chen
+      </Typography>
+      <Typography variant="body1" align="center">
+        Development Lead: Rahul Deb
       </Typography>
     </Box>
   );
