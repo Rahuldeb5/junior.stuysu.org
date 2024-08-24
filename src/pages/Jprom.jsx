87be4a9dbfe9b6c628d917c71a553219f4d7164b
Rahuldeb5 <rahuldeb.usa@gmail.com>
2024-08-24 11:06:29 -0400

    Added events pages

--- src/pages/Jprom.jsx
+++ src/pages/Jprom.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import "./Jprom.css";
 import Navbar from "../comps/Navbar";
 
@@ -7,6 +7,21 @@ export default function Jprom() {
     <Box className="jprom-page">
       <Box className="navbar">
         <Navbar {...{ page: "Jprom" }} />
+      </Box>
+
+      <Box className="content">
+        <Box className="info">
+          <Typography variant="h1" className="header">
+            Our Fundraising Initiatives:
+          </Typography>
+          <br />
+          <Typography variant="body" className="desc">
+            For Junior Prom to be as affordable as possible, Junior Caucus has
+            several initiatives dedicated towards fundraising to contribute to
+            this cause. The intended goal is to raise $15,000 dollars to lower
+            costs for around $50 per student.
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
--- src/pages/OtherEvents.jsx
+++ src/pages/OtherEvents.jsx
@@ -1,12 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import "./OtherEvents.css";
 import Navbar from "../comps/Navbar";
 
 export default function OtherEvents() {
   return (
-    <Box className="other-events-page">
+    <Box className="other-page">
       <Box className="navbar">
         <Navbar {...{ page: "OtherEvents" }} />
+      </Box>
+      <Box className="content">
+        <Box className="info">
+          <Typography variant="h1" className="title">
+            No Events Yet!
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
